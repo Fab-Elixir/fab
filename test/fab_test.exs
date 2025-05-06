a43b8b169bf8fdaaa73880d1525b8a2020ead0f3
Anthony Smith <420061+anthonator@users.noreply.github.com>
2025-05-06 15:56:53 -0400

    chore: set seed when running doctests

--- test/fab_test.exs
+++ test/fab_test.exs
@@ -1,8 +1,12 @@
 defmodule FabTest do
   use ExUnit.Case
 
-  setup do
+  setup tags do
+    seed = :binary.decode_unsigned(to_string(tags[:test]))
+
     Application.put_env(:fab, :consistent, true)
+    Application.put_env(:fab, :seed, seed)
+
     :ok
   end
 
