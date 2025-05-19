38d40668d466dc50f6122f8be3ee674876cdbc7e
Anthony Smith <420061+anthonator@users.noreply.github.com>
2025-05-19 12:21:11 -0400

    feat: implemented generator templates (#45)
    * chore: added `:eex` to `:extra_applications` list
    
    * feat: implemented generator templates

---
+++ test/fab/template_test.exs
@@ -0,0 +1,24 @@
+defmodule Fab.TemplateTest do
+  use ExUnit.Case, async: true
+
+  test "render/1" do
+    template =
+      %Fab.Template{
+        bindings: [
+          b1: {__MODULE__, :b1, []},
+          b2: {__MODULE__, :b2, []}
+        ],
+        source: "<%= b1 %> <%= b2 %>"
+      }
+
+    assert "Hello World" == Fab.Template.render(template)
+  end
+
+  def b1 do
+    "Hello"
+  end
+
+  def b2 do
+    "World"
+  end
+end
